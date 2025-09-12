b871f18a0f37b23f93e2519f4a4d612b0c0f9255
eren8204 <apskumar24@gmail.com>
2025-09-12 18:48:20 +0530

    - Build Fix3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,14 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.4.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+    }
+}
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android") version "2.2.20"
