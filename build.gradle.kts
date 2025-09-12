eebe87a94b3e77b0d6c007e1ec4030371d7e5211
eren8204 <apskumar24@gmail.com>
2025-09-12 17:38:43 +0530

    - Fixing Build

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,12 +28,17 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 }
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -43,25 +48,11 @@ afterEvaluate {
     publishing {
         publications {
             create<MavenPublication>("release") {
-                val releaseComponent = components.findByName("release")
-                if (releaseComponent != null) {
-                    from(releaseComponent)
-                }
-
+                from(components["release"])
                 groupId = "com.github.eren8204"
                 artifactId = "pdfviewer"
                 version = "1.0.0"
-                artifact(tasks.named("sourcesJar"))
-                artifact(tasks.named("javadocJar"))
             }
         }
     }
-}
-tasks.register<Jar>("sourcesJar") {
-    archiveClassifier.set("sources")
-    from(android.sourceSets["main"].java.srcDirs)
-}
-
-tasks.register<Jar>("javadocJar") {
-    archiveClassifier.set("javadoc")
 }
